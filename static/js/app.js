b30b8f53539040bdaf99f709a863e079f2cc5469
Ravinder Sollet <solletravinder@gmail.com>
2019-07-01 15:23:12 +0530

    changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,5 @@
 'use strict'
 var myApp = angular.module('myautocomplete', [
-    'ngCookies',
     'angucomplete-alt',
 ]);
 myApp.controller('search_for_value', ['$scope', '$http', '$location', "$rootScope", function($scope, $http, $location, $rootScope){
@@ -10,18 +9,18 @@ myApp.controller('search_for_value', ['$scope', '$http', '$location', "$rootScop
         return result
     }
 }]);
-myApp.run(function ($http, $cookies) {
-    // console.log($cookies.getAll())
-    // console.log($cookies.get('csrftoken'))
-    if($cookies.csrftoken){
-        $http.defaults.headers.common['X-CSRFToken'] = $cookies.csrftoken;
-    }else{
-        var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
-        $http.defaults.headers.common['X-CSRFToken'] = csrftoken
-    }
-    console.log($http.defaults.headers.common)
+// myApp.run(function ($http, $cookiesProvider) {
+//     // console.log($cookies.getAll())
+//     // console.log($cookies.get('csrftoken'))
+//     if($cookiesProvider.csrftoken){
+//         $http.defaults.headers.common['X-CSRFToken'] = $cookiesProvider.csrftoken;
+//     }else{
+//         var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+//         $http.defaults.headers.common['X-CSRFToken'] = csrftoken
+//     }
+//     console.log($http.defaults.headers.common)
 
-});
+// });
 myApp.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
